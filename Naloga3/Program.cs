521eabc5b5187d95d3e08a3cece34210b28bf334
Silvester Markovič <silvester@tomas.si>
2020-11-24 12:26:02 +0100

    Add project files.

---
+++ Naloga3/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Naloga3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Namen naloge je spoznati podatkovne strukture
+            
+
+            //TODO. 3.5 odkomentiraj
+            LinkedList<Kupec> seznam = new LinkedList<Kupec>();
+
+            //TODO: 3.5 Dodajte kupce v seznam
+            //Dodajte 3 obicajne kupce, 2 pogodbea in 2 extra
+            dodajNaSeznam(new Kupec() { naziv = "Ime", extra = false, pogodba = false }, seznam);
+            dodajNaSeznam(new Kupec() { naziv = "Ime2", extra = false, pogodba = false }, seznam);
+            dodajNaSeznam(new Kupec() { naziv = "ExtraDona", extra = true, pogodba = false }, seznam);
+            dodajNaSeznam(new Kupec() { naziv = "Ime3", extra = false, pogodba = false }, seznam);
+            dodajNaSeznam(new Kupec() { naziv = "ImePogodba1", extra = false, pogodba = true }, seznam);
+            dodajNaSeznam(new Kupec() { naziv = "ImePogodba2", extra = false, pogodba = true }, seznam);
+            dodajNaSeznam(new Kupec() { naziv = "ExtraDona", extra = true, pogodba = false }, seznam);
+            dodajNaSeznam(new Kupec() { naziv = "Ime5", extra = false, pogodba = false }, seznam);
+
+            //TODO: 3.6 Kliči metodo izpisi seznam
+            //izpisi seznam, kliči metodo
+            izpisSeznam(seznam);
+        }
+
+
+        //TODO: 3.2 ustvari metodo dodajNaSeznam
+        //metoda doda, kupca na seznam, na točno določeno mesto, na podlagi pravil
+        //če je extra nazačetek seznama
+        //če je pogodba na drugo mesto
+        public static void dodajNaSeznam(Kupec pkupec, LinkedList<Kupec>  pseznam)
+        {
+            if (pkupec.extra==true)
+            {
+                pseznam.AddFirst(pkupec);
+                return;
+            } else if (pkupec.pogodba == false || pseznam.Count < 1)
+            {
+                pseznam.AddLast(pkupec);
+                return;
+            } else
+            {
+                //Pogodben dodamo na drugo mesto
+                pseznam.AddAfter( pseznam.First, pkupec);
+                
+            }
+           
+        }
+
+
+        //TODO: 3.3 ustvari metodo kupecObdelan
+        //metoda kupecObdelan  (odstrani prvi element), kot parameter prejme seznam
+        //namig ena vrstica
+        public static void kupecObdelan(LinkedList<Kupec> pseznam)
+        {
+            pseznam.RemoveFirst();
+        }
+
+        //TODO: 3.5 Izpisite seznam
+        //izpisite seznam
+        public static void izpisSeznam(LinkedList<Kupec> pseznam)
+        { 
+            foreach (Kupec kup in pseznam)
+            {
+                Console.WriteLine($"{kup.naziv}");
+        }
+    }
+}
+
+
+
+    //razred Kupec
+    class Kupec
+    {
+        public string naziv { get; set; }
+        public bool pogodba { get; set; }
+        public bool extra { get; set; }
+
+        
+
+    }
+}
---
+++ Vaje4_Predpriprava/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vaje4_Predpriprava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating HashSet 
+            // Using HashSet class 
+            HashSet<string> seznam1 = new HashSet<string> { "Test1", "Test2", "Test3"};
+
+            // Add the elements in HashSet 
+            // Using Add method 
+            seznam1.Add("Marko");
+            seznam1.Add("Primoz");
+            seznam1.Add("Zak");
+            seznam1.Add("Pak");
+            seznam1.Add("Mak");
+             
+            // Creating another HashSet 
+            // Using HashSet class 
+            HashSet<string> seznam2 = new HashSet<string>();
+            // Add the elements in HashSet 
+            // Using Add method 
+            seznam2.Add("Matej");
+            seznam2.Add("Ivan");
+            seznam2.Add("Lan");
+            seznam2.Add("Marko");
+            seznam2.Add("Jaka");
+            seznam2.Add("Mak");
+
+            // Creating a new HashSet that contains 
+            // the union of both the HashSet mySet1 & mySet2 
+            HashSet<string> seznamUnija= new HashSet<string>(seznam1);
+            seznamUnija.UnionWith(seznam2);
+
+            izpisSeznam(seznam1, "Seznam1");
+            izpisSeznam(seznam2, "Seznam2");
+            izpisSeznam(seznamUnija, "Unija");
+
+            // Using IntersectWith method 
+            HashSet<string> seznamPresek = new HashSet<string>(seznam1);
+            seznamPresek.IntersectWith(seznam2);
+            izpisSeznam(seznamPresek, "Presek");
+
+            // After using Remove method 
+            Console.WriteLine("Total number of elements present" +
+                                " in myhash: {0}", seznam1.Count);
+
+
+            // Remove element from HashSet 
+            // Using Remove method 
+            seznam1.Remove("Ruby");
+
+            // Before using Remove method 
+            Console.WriteLine("Total number of elements present" +
+                                " in myhash: {0}", seznam1.Count);
+
+            // Remove all elements from HashSet 
+            // Using Clear method 
+            seznam1.Clear();
+            Console.WriteLine("Total number of elements present" +
+                                 " in myhash:{0}", seznam1.Count);
+
+
+
+        }
+
+        static void izpisSeznam(HashSet<String> pseznam, string naziv)
+        {
+
+            Console.WriteLine(naziv);
+            foreach (var ele in pseznam)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+}
